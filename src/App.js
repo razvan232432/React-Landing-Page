3eaf35b1dd17d701a3b51c15b1b7867581db3b73
razvan232432 <marius3837@proton.me>
2023-07-13 04:37:32 -0700

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          <table>
+            <tr> 
+            <th>Home</th>
+            <th>Pricing</th>
+            <th>About</th> 
+            <th>Support</th>
+            </tr>
+          </table>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
